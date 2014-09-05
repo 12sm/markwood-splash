af06c6b2c4659e1fb5d2dfcfbeb096e8bfa77afe
Michael Shoup <michael@12southmusic.com>
2014-09-05 14:08:50 -0400

    initial commit

---
+++ templates/content-page-home.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <script type="text/javascript" src="//use.typekit.net/fez7qrq.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+    <meta charset="utf-8">
+    <title>The Method</title>
+  </head>
+  <body id="container" class="grey">
+    <div class="positioner">
+      <div class="row" id="soon">
+        <div class="col-sm-12">
+          <h1>COMING SOON</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-sm-12">
+          <h4>Enter your email to access the video preview:</h4>
+        </div>
+      </div>
+      <div class="row">
+        <form action="submit">
+          <div class="col-sm-12">
+            <input type="text" name="email">
+          </div>
+          <div class="atag col-sm-12">
+            <a type="button" id="submitEmail"><img src="wp-content/themes/splash/assets/img/arrow-left.png">SUBMIT<img src="wp-content/themes/splash/assets/img/arrow-right.png"></a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
