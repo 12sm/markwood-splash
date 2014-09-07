b16f2b43516ba9bc327373cecdf171acc20a8a0e
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-07 14:32:50 -0500

    bg

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -9,21 +9,21 @@
   <body id="container" class="grey">
     <div class="positioner">
       <div class="row" id="soon">
-        <div class="col-sm-12">
+        <div class="col-xs-12">
           <h1>COMING SOON</h1>
         </div>
       </div>
       <div class="row">
-        <div class="col-sm-12">
+        <div class="col-xs-12">
           <h4>Enter your email to access the video preview:</h4>
         </div>
       </div>
       <div class="row">
         <form action="submit">
-          <div class="col-sm-12">
+          <div class="col-xs-12">
             <input type="text" name="email">
           </div>
-          <div class="atag col-sm-12">
+          <div class="atag col-xs-12">
             <img src="wp-content/themes/splash/assets/img/arrow-left.png"><a type="button" id="submitEmail">SUBMIT</a><img src="wp-content/themes/splash/assets/img/arrow-right.png">
           </div>
         </form>
