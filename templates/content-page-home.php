030a02e96b3384fae9b07b9fc617d35ea2422d17
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-07 23:25:26 -0500

    not

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -6,7 +6,7 @@
     <meta charset="utf-8">
     <title>The Method</title>
   </head>
-  <body class="grey">
+  <body id="container" class="grey">
     <div class="positioner">
       <div class="row" id="soon">
         <div class="col-xs-12">
