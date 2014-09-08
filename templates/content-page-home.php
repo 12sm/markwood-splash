f997cee8c41224c752fd5a2840377fbf46710cb1
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-08 00:39:50 -0500

    comes

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,25 +1,27 @@
 
 <script type="text/javascript" src="//use.typekit.net/fez7qrq.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-<div id="positioner">
-  <div class="row" id="soon">
-    <div class="col-xs-12">
-      <h1>COMING SOON</h1>
-    </div>
-  </div>
-  <div class="row">
-    <div class="col-xs-12">
-      <h4>Enter your email to access the video preview:</h4>
-    </div>
-  </div>
-  <div class="row">
-    <form action="submit">
+<div class="shell">
+  <div id="positioner">
+    <div class="row" id="soon">
       <div class="col-xs-12">
-        <input type="text" name="email">
+        <h1>COMING SOON</h1>
       </div>
-      <div class="atag col-xs-12">
-        <img src="wp-content/themes/splash/assets/img/arrow-left.png"><a type="button" id="submitEmail">SUBMIT</a><img src="wp-content/themes/splash/assets/img/arrow-right.png">
+    </div>
+    <div class="row">
+      <div class="col-xs-12">
+        <h4>Enter your email to access the video preview:</h4>
       </div>
-    </form>
+    </div>
+    <div class="row">
+      <form action="submit">
+        <div class="col-xs-12">
+          <input type="text" name="email">
+        </div>
+        <div class="atag col-xs-12">
+          <img src="wp-content/themes/splash/assets/img/arrow-left.png"><a type="button" id="submitEmail">SUBMIT</a><img src="wp-content/themes/splash/assets/img/arrow-right.png">
+        </div>
+      </form>
+    </div>
   </div>
 </div>
