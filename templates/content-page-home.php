309eb8ff8d020891c698110f406f7fefa66d330f
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-08 15:56:55 -0500

    s

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -1,6 +1,6 @@
 <script type="text/javascript" src="//use.typekit.net/fez7qrq.js"></script>
 <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
-<div class="container shell">
+<div class="shell">
   <div id="positioner">
     <div class="row" id="soon">
       <div class="col-xs-12">
