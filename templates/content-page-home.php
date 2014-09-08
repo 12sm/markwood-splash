a2d7d791ce3a63546661ed10138825fc3037ae27
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-07 23:31:27 -0500

    to

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -7,26 +7,28 @@
     <title>The Method</title>
   </head>
   <body id="container" class="grey">
-    <div class="positioner">
-      <div class="row" id="soon">
-        <div class="col-xs-12">
-          <h1>COMING SOON</h1>
-        </div>
-      </div>
-      <div class="row">
-        <div class="col-xs-12">
-          <h4>Enter your email to access the video preview:</h4>
-        </div>
-      </div>
-      <div class="row">
-        <form action="submit">
+    <div id="shell">
+      <div class="positioner">
+        <div class="row" id="soon">
           <div class="col-xs-12">
-            <input type="text" name="email">
+            <h1>COMING SOON</h1>
           </div>
-          <div class="atag col-xs-12">
-            <img src="wp-content/themes/splash/assets/img/arrow-left.png"><a type="button" id="submitEmail">SUBMIT</a><img src="wp-content/themes/splash/assets/img/arrow-right.png">
+        </div>
+        <div class="row">
+          <div class="col-xs-12">
+            <h4>Enter your email to access the video preview:</h4>
           </div>
-        </form>
+        </div>
+        <div class="row">
+          <form action="submit">
+            <div class="col-xs-12">
+              <input type="text" name="email">
+            </div>
+            <div class="atag col-xs-12">
+              <img src="wp-content/themes/splash/assets/img/arrow-left.png"><a type="button" id="submitEmail">SUBMIT</a><img src="wp-content/themes/splash/assets/img/arrow-right.png">
+            </div>
+          </form>
+        </div>
       </div>
     </div>
   </body>
