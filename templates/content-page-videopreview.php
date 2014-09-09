e301123770a8d63818bb3d24de2c3ba9569af2fb
Spencer Forrest Anastasio <anastasio.spencer@gmail.com>
2014-09-09 11:20:34 -0500

    calls fitvids on video

--- templates/content-page-videopreview.php
+++ templates/content-page-videopreview.php
@@ -1,13 +1,13 @@
 <div class="previewshell">
   <div id="previewpositioner">
     <div class="row">
-      <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
+      <div class="col-xs-8 col-xs-offset-2 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
         <img class="logo" src="../wp-content/themes/splash/assets/img/logo.png">
       </div>
     </div>
     <div class="frame row">
-      <div class="col-xs-12 col-sm-6 col-sm-offset-3 col-md-4 col-md-offset-4">
-        <iframe src="https://www.youtube.com/embed/IukYi79V7jA"></iframe>
+      <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+        <iframe id="video" src="https://www.youtube.com/embed/IukYi79V7jA"></iframe>
       </div>
     </div>
     <div class="row">
