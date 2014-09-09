f96688a869f033c1a99f06e4a84c569040bac3cf
michael <michael@12southmusic.com>
2014-09-09 13:35:44 -0500

    Enter MailChimp signup form

--- templates/content-page-home.php
+++ templates/content-page-home.php
@@ -11,12 +11,20 @@
       </div>
     </div>
     <div class="row">
-      <form action="submit">
+      <form action="//electrifyyourstrings.us9.list-manage.com/subscribe/post?u=ed36aed8dc2f652b6704be7e7&amp;id=97a4021445" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
         <div class="emailbox col-xs-12">
-          <input type="text" name="email">
+          <input type="email" value="" name="EMAIL" class="required email" id="mce-EMAIL">
         </div>
+        <div id="mce-responses" class="clear">
+		<div class="response" id="mce-error-response" style="display:none"></div>
+		<div class="response" id="mce-success-response" style="display:none"></div>
+	</div>
+	<div style="position: absolute; left: -5000px;"><input type="text" name="b_ed36aed8dc2f652b6704be7e7_97a4021445" tabindex="-1" value=""></div>
         <div class="atag col-xs-12">
-          <img src="wp-content/themes/splash/assets/img/arrow-left.png"><a href="videopreview" type="button" id="submitEmail">SUBMIT</a><img src="wp-content/themes/splash/assets/img/arrow-right.png">
+          <img src="wp-content/themes/splash/assets/img/arrow-left.png">
+          		<input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button">
+          		<a href="videopreview" type="button" id="submitEmail">SUBMIT</a>
+          <img src="wp-content/themes/splash/assets/img/arrow-right.png">
         </div>
       </form>
     </div>
